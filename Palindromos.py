e32b3175d1be9c18c43c505c97b6f0d9b8c9ada9
nicolasventin <nve2004@gmail.com>
2023-04-11 10:28:12 -0300

    Exercise finished with details in it

---
+++ Palindromos.py
@@ -0,0 +1,46 @@
+import unittest
+def palindrome(word):
+    word = word.lower()
+    a = 0             # 0 Es la primer letra de la palabra
+    b = len(word)-1   # para buscar la ultima letra de la palabra busco el largo len() y le resto 1 lo cual me da la ultima letra
+    
+    for i in range(0, len(word)): 
+        
+        #creo un rango entre 0 y el largo de la palabra
+        
+        if word [a] == word [b]: 
+            #verificamos si a es = a y b, es decir verificamos si la primer letra es igual a la ultima
+           a += 1 
+            #Si verifica la primer letra que pase a la siguiente
+            
+           b -= 1 
+            #si verifica la ultima letra que pase a la penultima y asi hasta terminar la palabra
+        
+        else:
+            return False
+    return True
+
+
+class TestPalindrome(unittest.TestCase):
+    
+    def test_1 (self):
+        result = palindrome('aba')
+        self.assertEqual(result, True)
+    
+    def test_2 (self):
+        result = palindrome('Neuquen')
+        self.assertEqual(result, True)
+    
+    def test_3 (self):
+        result = palindrome('vaca')
+        self.assertEqual(result, False)
+
+    def test_4 (self):
+        result = palindrome('balab')
+        self.assertEqual(result, True)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
